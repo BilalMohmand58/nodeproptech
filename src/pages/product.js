84193d467f7c89444e17dba88117a4ffe91abd86
CoderMasterypro <abdulrehmanabbasi5067794@gmail.com>
2024-06-04 21:25:01 -0700

    UpdateProductPAge

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,6 +1,7 @@
 import ProductServices from "@/components/ProductServices";
 import { getMetadata } from "@/config/metadata";
 import Layout from "@/layout/Layout";
+import PageBanner from "@/layout/PageBanner";
 import AboutComponent from "@components/AboutComponent";
 import BlogComponent from "@components/BlogComponent";
 import Drops from "@components/Drops";
@@ -19,8 +20,17 @@ const Product = () => {
         <meta name="description" content={description} />
       </Head>
       <Layout pageName={"Product"}>
-        <KendurnsHero />
+        {/* <KendurnsHero /> */}
         {/* !Hero Header */}
+        <PageBanner
+          pageName={"Product"}
+          pagetext={
+            " Find the perfect path to owning premium real estate with just a click."
+          }
+          // pagetext2={
+          //   "Fill out the form below, and our team will contact you immediately"
+          // }
+        />
         {/* About Section */}
         <AboutComponent />
         {/* !About Section */}
