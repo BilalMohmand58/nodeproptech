61ddf86928d61b43d11d5443f66f9b22677c70dd
CoderMasterypro <abdulrehmanabbasi5067794@gmail.com>
2024-07-02 00:15:28 -0700

    Finall

--- src/pages/half-kenburns.js
+++ src/pages/half-kenburns.js
@@ -1,29 +1,37 @@
-import Layout from "@/layout/Layout";
-import AboutComponent from "@components/AboutComponent";
-import BlogComponent from "@components/BlogComponent";
-import Drops from "@components/Drops";
-import HalfKenburnsHero from "@components/HalfKenburnsHero";
-import ServicesComponent from "@components/ServicesComponent";
-import WhyChooseUsComponent from "@components/WhyChooseUsComponent";
+// import Layout from "@/layout/Layout";
+// import AboutComponent from "@components/AboutComponent";
+// import BlogComponent from "@components/BlogComponent";
+// import Drops from "@components/Drops";
+// import HalfKenburnsHero from "@components/HalfKenburnsHero";
+// import ServicesComponent from "@components/ServicesComponent";
+// import WhyChooseUsComponent from "@components/WhyChooseUsComponent";
+
+// const HalfKenburns = () => {
+//   return (
+//     <Layout pageName={"Home Half Kenburns"}>
+//       <HalfKenburnsHero />
+//       {/* !Hero Header */}
+//       {/* About Section */}
+//       <AboutComponent />
+//       {/* !About Section */}
+//       {/* Services Section */}
+//       <ServicesComponent />
+//       {/* !Services Section */}
+//       <Drops />
+//       {/* Investor Section */}
+//       <WhyChooseUsComponent />
+//       {/* !Investor Section */}
+//       {/* Blog Section */}
+//       <BlogComponent />
+//     </Layout>
+//   );
+// };
+// export default HalfKenburns;
+
+import React from "react";
 
 const HalfKenburns = () => {
-  return (
-    <Layout pageName={"Home Half Kenburns"}>
-      <HalfKenburnsHero />
-      {/* !Hero Header */}
-      {/* About Section */}
-      <AboutComponent />
-      {/* !About Section */}
-      {/* Services Section */}
-      <ServicesComponent />
-      {/* !Services Section */}
-      <Drops />
-      {/* Investor Section */}
-      <WhyChooseUsComponent />
-      {/* !Investor Section */}
-      {/* Blog Section */}
-      <BlogComponent />
-    </Layout>
-  );
+  return <div>HalfKenburns</div>;
 };
+
 export default HalfKenburns;
--- src/pages/kenburns.js
+++ src/pages/kenburns.js
@@ -1,29 +1,36 @@
-import Layout from "@/layout/Layout";
-import AboutComponent from "@components/AboutComponent";
-import BlogComponent from "@components/BlogComponent";
-import Drops from "@components/Drops";
-import KendurnsHero from "@components/KendurnsHero";
-import ServicesComponent from "@components/ServicesComponent";
-import WhyChooseUsComponent from "@components/WhyChooseUsComponent";
+// import Layout from "@/layout/Layout";
+// import AboutComponent from "@components/AboutComponent";
+// import BlogComponent from "@components/BlogComponent";
+// import Drops from "@components/Drops";
+// import KendurnsHero from "@components/KendurnsHero";
+// import ServicesComponent from "@components/ServicesComponent";
+// import WhyChooseUsComponent from "@components/WhyChooseUsComponent";
+
+// const Kenburns = () => {
+//   return (
+//     <Layout pageName={"Home Kenburns"}>
+//       <KendurnsHero />
+//       {/* !Hero Header */}
+//       {/* About Section */}
+//       <AboutComponent />
+//       {/* !About Section */}
+//       {/* Services Section */}
+//       <ServicesComponent />
+//       {/* !Services Section */}
+//       {/* <Drops /> */}
+//       {/* Investor Section */}
+//       <WhyChooseUsComponent />
+//       {/* !Investor Section */}
+//       {/* Blog Section */}
+//       <BlogComponent />
+//     </Layout>
+//   );
+// };
+// export default Kenburns;
+import React from "react";
 
 const Kenburns = () => {
-  return (
-    <Layout pageName={"Home Kenburns"}>
-      <KendurnsHero />
-      {/* !Hero Header */}
-      {/* About Section */}
-      <AboutComponent />
-      {/* !About Section */}
-      {/* Services Section */}
-      <ServicesComponent />
-      {/* !Services Section */}
-      {/* <Drops /> */}
-      {/* Investor Section */}
-      <WhyChooseUsComponent />
-      {/* !Investor Section */}
-      {/* Blog Section */}
-      <BlogComponent />
-    </Layout>
-  );
+  return <div>Kenburns</div>;
 };
+
 export default Kenburns;
