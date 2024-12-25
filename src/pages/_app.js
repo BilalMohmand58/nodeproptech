47eafbd346d0f7755b40adeca25ebe869e5b4b7a
Bilal Ahmad <bilalahmadkhan1729@gmail.com>
2024-12-25 16:37:15 +0500

    ga4 tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,59 @@
-// _app.js
-import { Fragment } from "react";
+// // _app.js
+// import { Fragment } from "react";
+// import "../styles/globals.css";
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <Fragment>
+//       <Component {...pageProps} />
+//     </Fragment>
+//   );
+// }
+
+// export default MyApp;
+
+import { Fragment, useEffect } from "react";
+import { useRouter } from "next/router";
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (window.gtag) {
+        window.gtag("config", "G-8YLPYCMEJX", {
+          page_path: url,
+        });
+      }
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Fragment>
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-8YLPYCMEJX`}
+      />
+
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-8YLPYCMEJX', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </Fragment>
   );
